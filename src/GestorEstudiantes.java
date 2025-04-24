42d67555966651cee4c9190d586ea45cf200e2b6
Lucks73 <thelucasmthompson@gmail.com>
2025-04-25 00:15:03 +0200

    Error 3 resuelto.

--- src/GestorEstudiantes.java
+++ src/GestorEstudiantes.java
@@ -12,7 +12,7 @@ public class GestorEstudiantes {
         }
         if(estudiante.getNotas().length == 0)
         {
-            return 0;
+            return -1;
         }else {
             return suma / estudiante.getNotas().length;
         }
@@ -21,25 +21,38 @@ public class GestorEstudiantes {
 
     // Encuentra al estudiante con la mejor nota media
     public static Estudiante encontrarMejorEstudiante(Estudiante[] estudiantes) {
+        estudiantes = null;
         Estudiante mejor = null;
         double mejorNota = -1;
-
-        for (Estudiante estudiante : estudiantes) {
-            double media = calcularNotaMedia(estudiante); // Posible fallo aquí
-            if (media > mejorNota) {
-                mejorNota = media;
-                mejor = estudiante;
+        try{
+            for (Estudiante estudiante : estudiantes) {
+                double media = calcularNotaMedia(estudiante); // Posible fallo aquí
+                if (media > mejorNota) {
+                    mejorNota = media;
+                    mejor = estudiante;
+                }
             }
+            return mejor;
+        }catch (NullPointerException e)
+        {
+            System.out.println("La lista de estudiantes esta vacia o nula.");
+            return null;
         }
-        return mejor; // Error si la lista está vacía
     }
 
     // Guarda los resultados en un fichero
     public static void guardarResultados(Estudiante[] estudiantes, String rutaFichero) {
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(rutaFichero))) {
             for (Estudiante estudiante : estudiantes) {
-                writer.write("Nombre: " + estudiante.getNombre() + ", Nota Media: " +
-                        calcularNotaMedia(estudiante)); // Posible fallo si calcularNotaMedia lanza una excepción
+
+                if(calcularNotaMedia(estudiante) == -1)
+                {
+                    writer.write("Nombre: " + estudiante.getNombre() + " // SIN NOTAS" );
+                }
+                else {
+                    writer.write("Nombre: " + estudiante.getNombre() + ", Nota Media: " +
+                            calcularNotaMedia(estudiante));
+                }
                 writer.newLine();
             }
         } catch (IOException e) {
